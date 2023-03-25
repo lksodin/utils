32ef6507b01a57689a821fafb5c4f3da802a4e68
odie <odie.jh@outlook.com>
2023-03-25 17:44:37 +0800

    init

---
+++ DomainListHelper.php
@@ -0,0 +1,78 @@
+<?php
+namespace Od\Utils;
+
+use Curl\Curl;
+
+class DomainListHelper
+{
+    const API_ENDPOINT = 'https://api.domains.ccgert.com/api/getAllDomain';
+
+    private $input_params = [];
+    private $default_params = [
+        'level2' => 1,
+        'schema' => 0,
+    ];
+    private $require_params = [ 'site', 'type', 'status' ];
+    private $params_key_list = [ 'status', 'site', 'type' ];
+
+    public function __construct(array $params)
+    {
+        $this->validate($params);
+        $this->input_params = $params;
+    }
+
+    public function getList()
+    {
+        $curl = new Curl;
+        $payload = array_merge($this->default_params, $this->input_params);
+        $response = $curl->get(self::API_ENDPOINT, $payload);
+        $curl_info = $curl->getInfo();
+
+        if ($curl->getHttpStatusCode() == 200) {
+            $array_response = self::responseToArray($response);
+
+            if (isset($array_response['data'][$payload['site']][$payload['type']])) {
+                $this->domain_list = $array_response['data'][$payload['site']][$payload['type']];
+
+                return $this->domain_list;
+            } else {
+                throw new Exception("empty domain list returned: [{$raw_res}]");
+            }
+        } else {
+            throw new Exception("Get domain list failed, Unexpected api response: [{$response}]");
+        }
+    }
+
+    private function responseToArray($response)
+    {
+        $array_res = json_decode(json_encode($response), true);
+
+        if (is_array($array_res)) {
+            return $array_res;
+        } else {
+            throw new Exception("unknown response:" . $response);
+        }
+    }
+
+    public function getWildcardDomainList()
+    {
+        if (empty($this->domain_list)) {
+            $this->getList();
+        }
+
+        return array_map(function ($domain) {
+            return "*.{$domain}";
+        }, $this->domain_list);
+    }
+
+    private function validate($params)
+    {
+        foreach ($this->params_key_list as $key) {
+            if (!array_key_exists($key, $params)) {
+                throw new Exception("missing params: [{$key}]");
+            }
+        }
+
+        return true;
+    }
+}
---
+++ DomainName.php
@@ -0,0 +1,87 @@
+<?php
+namespace Od\Utils;
+
+class DomainName
+{
+    const DEFAULT_SUB_LENGTH = 8;
+
+    private $level2;
+    private $sub;
+
+    public function __construct($domainName)
+    {
+        if ($domainName = filter_var($domainName, FILTER_VALIDATE_DOMAIN)) {
+            $domainNameParts = explode(".", $domainName);
+            $this->level2 = implode('.', array_slice($domainNameParts, -2, 2));
+            $this->sub = implode('.', array_slice($domainNameParts, 0, -2));
+        } else {
+            throw new InvalidArgumentException("Invalid domain name");
+        }
+    }
+
+    public function full()
+    {
+        return sprintf("%s.%s", $this->sub, $this->level2);
+    }
+
+    public function setLevel2($domainName)
+    {
+        if (explode('.', $domainName) != 2) {
+            throw new InvalidArgumentException("invalid format for level2 domain");
+        }
+
+        $this->level2 = $domainName;
+    }
+
+    public function level2()
+    {
+        return $this->level2;
+    }
+
+    public function setSub($sub)
+    {
+        $this->sub = $sub;
+    }
+
+    public function sub()
+    {
+        return $this->sub;
+    }
+
+    public function __toString()
+    {
+        return $this->full();
+    }
+
+    public function randomSub($length = self::DEFAULT_SUB_LENGTH, $random_pattern_mode = 0)
+    {
+        if (empty($this->sub)) {
+            throw new Exception("original domain type not specified");
+        }
+
+        if (empty($length)) {
+            $length = self::DEFAULT_SUB_LENGTH;
+        }
+
+        $length -= strlen($this->sub);
+        $prefix_length = floor($length / 2);
+        $random_str = md5(microtime(true));
+
+        //Todo: fix it
+        if ($random_pattern_mode !== 0) {
+            return sprintf(
+                "%s%s%s",
+                $this->sub[0],
+                substr($random_str, 0, $length),
+                $this->sub[1]
+            );
+        } else {
+            return sprintf(
+                "%s%s%s",
+                substr($random_str, 0, $prefix_length),
+                $this->sub,
+                substr($random_str, $prefix_length, $length - $prefix_length)
+            );
+        }
+    }
+}
