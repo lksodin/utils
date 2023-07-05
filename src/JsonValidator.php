811afe402f8eb1be0600e8e7b2e8d2e412e00036
odie <lksodin@gmail.com>
2023-07-05 16:10:49 +0800

    add JsonValidator

---
+++ src/JsonValidator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Od\Utils;
+
+class JsonValidator
+{
+    public static function isValid($string)
+    {
+        return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
+    }
+}
